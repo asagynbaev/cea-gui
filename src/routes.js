e91a37ee681b075c90efde51d608f020a2daae81
Azim Sangybaev <sangybaev@simplifate.com>
2019-11-25 22:52:18 +0100

    some changes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-const Charts = React.lazy(() => import('./views/Charts'));
 const Dashboard = React.lazy(() => import('./views/Dashboard'));
 const Users = React.lazy(() => import('./views/Users/Users'));
 const User = React.lazy(() => import('./views/Users/User'));
@@ -8,11 +7,9 @@ const AddUserForm = React.lazy(() => import('./views/Users/AddUserForm'));
 const EditUser = React.lazy(() => import('./views/Users/EditUser'));
 const Shifts = React.lazy(() => import('./views/Shifts/Shifts'));
 
-// https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
   { path: '/', exact: true, name: 'Главная' },
   { path: '/dashboard', name: 'Панель', component: Dashboard },
-  { path: '/charts', name: 'Charts', component: Charts },
   { path: '/users', exact: true,  name: 'Сотрудники', component: Users },
   { path: '/users/adduserform', name: 'Добавить сотрудника', component:  AddUserForm},
   { path: '/users/:id/edituser', name: 'Редактировать данные сотрудника', component:  EditUser},
